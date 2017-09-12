51c44c8592b8af2d322167c80576639a03ff5ec2
AlexZaurovna <AlexandraZaurovna@gmail.com>
2017-09-12 19:42:11 +0300

    first commit

---
+++ Lab1Var9.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+using namespace std;
+#include <stdio.h> 
+#include <Windows.h> 
+SetConsoleCP(1251);
+SetConsoleOutputCP(1251);
+//использование русского языка в тексте программы
+int main()
+{
+	int dm;
+	int hnd = dm * 0, 984252;
+	float vrst = hnd * 9, 52381e-5;
+	//введение переменных
+	cout « "Введите расстояние в дециметрах \n";
+	cin » dm;
+	//считывание длинны в дециметрах с клавиатуры
+	hnd = dm * 0, 984252;
+	cout « "Расстояние в ладонях равно ", hnd « endl;
+	//перевод в ладони и вывод на экран
+	vrst = hnd * 9, 52381e-5;
+	cout « "Расстояние в верстах равно ", vrst;
+	//перевод в вёрсты и вывод на экран
+	return 0;
+}
