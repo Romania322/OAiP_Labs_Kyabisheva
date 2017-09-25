8c3702a985a6bd990fc433b5bb3a2d519924928f
AlexZaurovna <31659078+AlexZaurovna@users.noreply.github.com>
2017-09-25 20:17:21 +0300

    OAiP

---
+++ OAiP_Labs_Kyabisheva/OAiP_Lab1Var9.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h> 
+#include <iostream>  
+#include <limits>
+using namespace std;
+
+int main()
+{ 	
+	system("chcp 1251");
+	system("cls");
+	int dm;
+	float hnd = dm * 0.984252;
+	float vrst = hnd * 9.52381e-5;
+	//Введение переменных
+	cout << "Введите расстояние в дециметрах" << endl;
+	cin >> dm;
+	//Считывание длинны в дециметрах с клавиатуры
+	if(!dm){
+		while(!(cin >> dm)){
+			cout << "Ошибка ввода.Повторите попытку" << endl;
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+	}
+	//Распознавание некоректных данных
+	hnd = dm * 0.984252;
+	cout << ("Расстояние в ладонях равно ", hnd) << endl;
+	//Перевод в ладони и вывод на экран
+	vrst = hnd * 9.52381e-5;
+	cout << ("Расстояние в верстах равно ", vrst) << endl;
+	//Перевод в версты и вывод на экран
+	system("pause");
+	return 0;
+}
