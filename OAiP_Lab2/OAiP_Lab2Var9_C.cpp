0129e52d146dfc5b7fd61ec5953f35b37ae7298c
AlexZaurovna <31659078+AlexZaurovna@users.noreply.github.com>
2017-10-31 19:35:01 +0300

    OAiP_Lab2

---
+++ OAiP_Lab2/OAiP_Lab2Var9_C.cpp
@@ -0,0 +1,57 @@
+/*
+Кябишева Александра Зауровна
+Лабораторная работа №2
+Вариант 9
+Задание: Написать программу, которая выводит на экран простые числа. Массив и его длина вводятся пользователем.
+*/
+#define _CRT_SECURE_NO_WARNINGS 
+ #include <stdio.h> 		
+ #include <conio.h> 		
+ #include <stdlib.h>		
+ 		
+ void function(int*address);
+ int main () {		
+ 		
+ 	system("chcp 1251");		
+ 	system("cls"); 		
+ 	int size_of_array;	 		
+ 	printf("Введите размер масива ");
+ 	function(&size_of_array);		
+ 	while (!(size_of_array > 0)) {		
+ 		function(&size_of_array);		
+ 	}		 		
+ 	int *array = (int*)malloc(size_of_array * sizeof(int));	 		
+ 	for (int i = 0; i < size_of_array; i++) {		
+ 		printf("Введите %d элемент массива ", i + 1);		
+ 		function(&array[i]);		
+ 	} 		
+ 	for (int i = 0; i < size_of_array; i++) {		
+ 		if(abs(array[i])<=3){		
+ 			printf("%d ", array[i]);		
+ 		}		
+ 		else {		
+ 			for (int j = 2; j < array[i]; j++) {		
+ 				if (array[i] % j == 0) {		
+ 					break;		
+ 				}		
+ 				if (j == array[i] - 1) {		
+ 					printf("%d ", array[i]);		
+ 				}		
+ 			}		
+ 		}		
+ 	}		
+ 	_getch();
+	 return 0;		
+ }	 		
+ void function(int*address) {		
+ 	int nmbr;		
+ 		while (!scanf("%d", &nmbr)) {		
+ 			printf("Ошибка ввода. Повторите попытку ");		
+ 			while (getchar() != '\n');		
+ 		}		
+ 	if (getchar() != '\n') {		
+ 		printf("Ошибка ввода.Повторите попытку ");		
+ 		function(&nmbr);		
+ 	}		
+ 	*address = nmbr;		
+}
