bac1b5e253ecc117a80a6f3382a8ac8c5cd2c853
AlexZaurovna <31659078+AlexZaurovna@users.noreply.github.com>
2017-11-02 09:39:34 +0300

    OAiP_Lab2_

--- OAiP_Lab2/OAiP_Lab2Var9_C.cpp
+++ OAiP_Lab2/OAiP_Lab2Var9_C.cpp
@@ -5,49 +5,49 @@
 Задание: Написать программу, которая выводит на экран простые числа. Массив и его длина вводится пользователем.
 */
 #define _CRT_SECURE_NO_WARNINGS 
- #include <stdio.h> 		
- #include <conio.h> 		
- #include <stdlib.h>		
- 		
- void function(int*address);
- int main () {
- 	system("chcp 1251");		
- 	system("cls"); 		
- 	int sizeOfArray;	 		
- 	printf("Введите размер массива ");
- 	function(&sizeOfArray);				 		
- 	int *array = (int*)malloc(sizeOfArray * sizeof(int));	 		
- 	for (int i = 0; i < sizeOfArray; i++) {		
- 		printf("Введите %d Элемент массива ", i + 1);		
- 		function(&array[i]);		
- 	} 		
- 	for (int i = 0; i < sizeOfArray; i++) {		
- 		if(abs(array[i]) <= 3) {		
- 			printf("%d ", array[i]);		
- 		}		
- 		else {		
- 			for (int j = 2; j < array[i]; j++) {		
- 				if (array[i] % j == 0) {		
- 					break;		
- 				}		
- 				if (j == array[i] - 1) {		
- 					printf("%d ", array[i]);		
- 				}		
- 			}		
- 		}		
- 	}		
- 	_getch();
-	 return 0;		
- }	 		
- void function(int*address) {		
- 	int number;		
- 	while (!scanf("%d", &number)) {		
- 		printf("Ошибка ввода. Повторите попытку ");		
- 		while (getchar() != '\n');		
- 	}		
- 	if (getchar() != '\n') {		
- 		printf("Ошибка ввода. Повторите попытку ");		
- 		function(&number);		
- 	}		
- 	*address = number;		
+#include <stdio.h> 		
+#include <conio.h> 		
+#include <stdlib.h>		
+
+void function(int*address);
+int main() {
+	system("chcp 1251");
+	system("cls");
+	int sizeOfArray;
+	printf("Введите размер массива ");
+	sizeOfArray = function();
+	int *array = (int*)malloc(sizeOfArray * sizeof(int));
+	for (int i = 0; i < sizeOfArray; i++) {
+		printf("Введите %d Элемент массива ", i + 1);
+		function(&array[i]);
+	}
+	for (int i = 0; i < sizeOfArray; i++) {
+		if (abs(array[i]) <= 3) {
+			printf("%d ", array[i]);
+		}
+		else {
+			for (int j = 2; j < array[i]; j++) {
+				if (array[i] % j == 0) {
+					break;
+				}
+				if (j == array[i] - 1) {
+					printf("%d ", array[i]);
+				}
+			}
+		}
+	}
+	_getch();
+	return 0;
+}
+int function() {
+	int number;
+	while (!scanf("%d", &number)) {
+		printf("Ошибка ввода. Повторите попытку ");
+		while (getchar() != '\n');
+	}
+	if (getchar() != '\n') {
+		printf("Ошибка ввода. Повторите попытку ");
+		function(&number);
+	}
+	return = number;
 }
